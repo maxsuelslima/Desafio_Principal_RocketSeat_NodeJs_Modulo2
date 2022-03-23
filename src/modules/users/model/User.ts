5506cffa0aff558c131bf20c80420ce59cc116d4
Maxsuel Lima <maxsuelslima@gmail.com>
2022-03-23 00:22:40 -0300

    update

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,20 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id: string;
+  name: string;
+  admin: boolean;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+  constructor(name:string,email:string){
+    this.id= uuidV4();
+    this.name= name;
+    this.admin= false;
+    this.email= email;
+    this.createdAt= new Date();
+    this.updatedAt= new Date();
+  }
 }
 
 export { User };
